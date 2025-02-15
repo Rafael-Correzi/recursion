21abb03283bb27f237e4d9eb4a9dad0029791ed8
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2025-02-14 21:32:45 -0300

    Modificar função fibs para retornar um array

--- fibonacci.js
+++ fibonacci.js
@@ -2,12 +2,27 @@ function fibs(num) {
   let fibOfNum = 1;
   let fibOfPreviousNum = 0
   let copy;
-  for (let i = 1; i < num; i++) {
+  let arr = [];
+  if (num >= 0 ) {
+    arr.push(0);
+  }
+  if (num >= 1 ) {
+    arr.push(1);
+  }
+  for (let i = 2; i < num; i++) {
     copy = fibOfNum;
     fibOfNum += fibOfPreviousNum;
     fibOfPreviousNum = copy;
+    arr.push(fibOfNum);
   }
-  return fibOfNum;
+  return arr;
 }
 
-console.log(fibs(50));
+console.log(fibs(10));
+
+/*function fibsRec(num) {
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  else
+}*/
